71574c5e3abefabbf78d38f5bc2018c9437c45eb
MedCy1 <MedDev1337@protonmail.com>
2023-06-06 15:57:26 +0200

    updates

--- backend/dashboard/dashboard.js
+++ backend/dashboard/dashboard.js
@@ -9,13 +9,13 @@ new Chart(chart, {
 
         datasets: [
             {
-                label: 'BTC',
+                label: 'EUR',
                 data: [29374, 33537, 49631, 59095, 57828, 36684, 33572, 39974, 48847, 48116, 61004],
                 borderColor: 'red',
                 borderWidth: 2
             },
             {
-                label: 'ETH',
+                label: 'DOL',
                 data: [31500, 41000, 88800, 26000, 46000, 32698, 5000, 3000, 18656, 24832, 36844],
                 borderColor: 'blue',
                 borderWidth: 2
